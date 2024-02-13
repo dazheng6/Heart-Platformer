19e1a3143d95ff8364eaeccc3cda9710b0f0af52
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2024-02-12 20:12:34 -0800

    progress check 3

--- Scripts/PlayerMovementData.gd
+++ Scripts/PlayerMovementData.gd
@@ -5,3 +5,5 @@ extends Resource
 @export var acceleration = 800.0
 @export var friction = 700.0
 @export var jump_velocity = -300.0
+@export var gravity_scale = 1.0
+@export var air_resistnace = 200.0
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,12 +8,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
 func _physics_process(delta):
-	movement_data = load("res://DefaultMovementData.tres")
 	apply_gravity(delta)
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
 	update_animatinos(input_axis)
 	var was_on_floor = is_on_floor()
 	move_and_slide()
@@ -21,9 +21,10 @@ func _physics_process(delta):
 	if just_left_ledge:
 		coyote_jump_timer.start()
 
+
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * movement_data.gravity_scale * delta
 		
 func handle_jump():
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
@@ -37,8 +38,12 @@ func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
 
+func apply_air_resistance(input_axis,delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistnace * delta)
+
 func apply_friction(input_axis, delta):
-	if input_axis == 0:
+	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
 func update_animatinos(input_axis):
 	if input_axis != 0:
