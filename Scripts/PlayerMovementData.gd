b4e7fc45327421df9ea366f952d29f2c6dd555dd
dazheng6 <121971803+dazheng6@users.noreply.github.com>
2024-02-15 14:31:18 -0800

    Finished to 12:12

--- Scripts/PlayerMovementData.gd
+++ Scripts/PlayerMovementData.gd
@@ -5,5 +5,6 @@ extends Resource
 @export var acceleration = 800.0
 @export var friction = 700.0
 @export var jump_velocity = -300.0
-@export var gravity_scale = 1.0
+@export var gravity_scale = 0.6
 @export var air_resistnace = 200.0
+@export var ait_acceleration = 400.0
